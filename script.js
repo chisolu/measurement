ba751488d74fbcccf869d6fb5f81fc1305532e3c
Ucheonye C . H Umeotawara <uchisolu@gmail.com>
2025-05-31 16:22:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function  measure(){
+   const playEL = document.getElementById('yesplay');
+   playEL.classList.add('my-cards-open');
+}
+
+
+function  closeup(){
+   const playEL = document.getElementById('yesplay');
+   playEL.classList.remove('my-cards-open');
+}
